07341cb75b511c1f50b057ec09840c9bf2bb52c9
Casey Sprague <casey@sprague.io>
2018-10-28 18:26:58 -0500

    Add additional apple-touch-icon sizes

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -3,10 +3,17 @@
 @section('head')
     <meta name=apple-mobile-web-app-capable content=yes>
     <meta name=mobile-web-app-capable content=yes>
-		<link rel="apple-touch-icon" href="{{ url('/img/bathroom-touch-icon.png') }}">
+	@if($checkoutType === 'Bathroom')
+        <link rel="apple-touch-icon" href="{{ url('/img/bathroom-touch-icon.png') }}">
+        <link rel="apple-touch-icon" sizes="152x152" href="{{ url('/img/bathroom-touch-icon.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ url('/img/bathroom-touch-icon.png') }}">
+        <link rel="apple-touch-icon" sizes="167x167" href="{{ url('/img/bathroom-touch-icon.png') }}">
         <meta name="apple-mobile-web-app-title" content="Bathroom">
-	@elseif($checkoutType == 'Library')
-		<link rel="apple-touch-icon" href="{{ url('/img/library-touch-icon.png') }}">
+	@elseif($checkoutType === 'Library')
+        <link rel="apple-touch-icon" href="{{ url('/img/library-touch-icon.png') }}">
+        <link rel="apple-touch-icon" sizes="152x152" href="{{ url('/img/library-touch-icon.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ url('/img/library-touch-icon.png') }}">
+        <link rel="apple-touch-icon" sizes="167x167" href="{{ url('/img/library-touch-icon.png') }}">
         <meta name="apple-mobile-web-app-title" content="Library">
 	@endif
 @endsection
